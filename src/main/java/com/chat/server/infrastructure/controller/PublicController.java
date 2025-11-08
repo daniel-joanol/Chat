f8a5c6ffc79f0edf43e0eb405bf7edd9064083ff
daniel-joanol <joanoldaniel@gmail.com>
2025-11-08 12:33:03 +0100

    Add login endpoint

--- src/main/java/com/chat/server/infrastructure/controller/PublicController.java
+++ src/main/java/com/chat/server/infrastructure/controller/PublicController.java
@@ -1,6 +1,8 @@
 package com.chat.server.infrastructure.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +14,11 @@ import com.chat.server.domain.model.User;
 import com.chat.server.domain.model.UserFactory;
 import com.chat.server.domain.service.UserService;
 import com.chat.server.infrastructure.controller.mapper.UserDtoMapper;
+import com.chat.server.infrastructure.controller.request.LoginRequest;
 import com.chat.server.infrastructure.controller.request.UserRequest;
 import com.chat.server.infrastructure.controller.response.UserResponse;
+import com.chat.server.infrastructure.exception.AuthenticationFailedException;
+import com.chat.server.infrastructure.exception.EntityNotFoundException;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -33,7 +38,35 @@ public class PublicController {
 
   private final UserService service;
   private final UserDtoMapper mapper;
-  
+
+  @Operation(
+      summary = "Login",
+      description = """
+          Endpoint to login.
+          Every parameter is mandatory.
+          """
+  )
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Login successful."),
+      @ApiResponse(responseCode = "400", description = "Mandatory parameter missing."),
+      @ApiResponse(responseCode = "401", description = "Unauthorized.")
+  })
+  @PostMapping("/login")
+  public ResponseEntity<String> login(
+      @Valid @RequestBody LoginRequest request
+  ) {
+    try {
+      String token = service.authenticate(request.username(), request.password());
+      return ResponseEntity
+          .status(HttpStatus.OK)
+          .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
+          .body(token);
+
+    } catch (EntityNotFoundException e) {
+      throw new AuthenticationFailedException(e.getInternalMessage(), e);
+    }
+  } 
+
   @Operation(
       summary = "Create user",
       description = """
--- src/main/java/com/chat/server/infrastructure/exception/AuthenticationFailedException.java
+++ src/main/java/com/chat/server/infrastructure/exception/AuthenticationFailedException.java
@@ -1,9 +1,15 @@
 package com.chat.server.infrastructure.exception;
 
 public class AuthenticationFailedException extends AbstractException {
+
+  private static final String externalMessage = "Authentication failed.";
   
   public AuthenticationFailedException(String internalMessage) {
-    super("Authentication failed.", internalMessage);
+    super(externalMessage, internalMessage);
+  }
+
+  public AuthenticationFailedException(String internalMessage, Throwable cause) {
+    super(externalMessage, internalMessage, cause);
   }
   
 }
