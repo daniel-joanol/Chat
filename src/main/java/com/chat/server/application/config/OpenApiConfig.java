9c7c377e429c7bb4f90c22d2df2babd2e5c02ca2
daniel-joanol <joanoldaniel@gmail.com>
2025-11-14 18:09:07 +0100

    Add token to controllers

--- src/main/java/com/chat/server/application/config/OpenApiConfig.java
+++ src/main/java/com/chat/server/application/config/OpenApiConfig.java
@@ -4,6 +4,11 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 @Configuration
 public class OpenApiConfig {
 
@@ -13,6 +18,19 @@ public class OpenApiConfig {
         .group("v1")
         .pathsToMatch("/**")
         .build();
+  }
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .components(new Components()
+            .addSecuritySchemes(
+                "bearerAuth",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
   }
   
 }
