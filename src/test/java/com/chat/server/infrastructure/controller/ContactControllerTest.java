ca8b8d79ae7bb4b25394a971de05607698469d2d
daniel-joanol <joanoldaniel@gmail.com>
2026-03-06 20:35:09 +0100

    Add tests to contact controller

---
+++ src/test/java/com/chat/server/infrastructure/controller/ContactControllerTest.java
@@ -0,0 +1,58 @@
+package com.chat.server.infrastructure.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.chat.server.domain.model.Contact;
+import com.chat.server.domain.service.ContactService;
+import com.chat.server.infrastructure.controller.mapper.ContactDtoMapper;
+import com.chat.server.infrastructure.controller.request.ContactRequest;
+
+@ExtendWith(MockitoExtension.class)
+class ContactControllerTest {
+  
+  EasyRandom generator = new EasyRandom();
+  ContactDtoMapper mapper = Mappers.getMapper(ContactDtoMapper.class);
+
+  @Mock
+  ContactService service;
+
+  @InjectMocks
+  ContactController sut;
+
+  @BeforeEach
+  void setUp() {
+    ReflectionTestUtils.setField(sut, "mapper", mapper);
+  }
+
+  @Test
+  void testAdd() {
+    var contact = generator.nextObject(Contact.class);
+    var request = new ContactRequest(contact.getFriend().getUsername());
+    when(service.addContact(any())).thenReturn(contact);
+    var response = sut.add(request);
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+  }
+
+  @Test
+  void testDelete() {
+    var id = UUID.randomUUID();
+    var response = sut.delete(id);
+    assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+  }
+
+}
