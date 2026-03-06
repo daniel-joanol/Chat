f6a6f518760b09a7a9c8bfe5da274341f9271e1f
daniel-joanol <joanoldaniel@gmail.com>
2026-03-06 21:11:46 +0100

    Improve readability

--- src/main/java/com/chat/server/application/service/DefaultContactService.java
+++ src/main/java/com/chat/server/application/service/DefaultContactService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.chat.server.domain.dao.ContactDao;
 import com.chat.server.domain.model.Contact;
+import com.chat.server.domain.model.User;
 import com.chat.server.domain.service.ContactService;
 import com.chat.server.domain.service.UserService;
 import com.chat.server.domain.util.SecurityUtil;
@@ -25,30 +26,30 @@ public class DefaultContactService implements ContactService {
   
   @Override
   public Contact addContact(String contactUsername) {
-    var username = secUtil.getUsername();
+    String username = secUtil.getUsername();
     if (contactUsername.equals(username)) {
-      var message = String.format("User %s trying to add itself", username);
+      String message = String.format("User %s trying to add itself", username);
       throw new BadRequestException(message);
     }
 
     if (dao.exists(username, contactUsername)) {
-      var message = String.format("Contact %s already associated to user %s", contactUsername, username);
+      String message = String.format("Contact %s already associated to user %s", contactUsername, username);
       throw new ConflictException(message);
     }
 
-    var user = userService.getByUsername(username);
-    var friend = userService.getByUsername(contactUsername);
-    var contact = Contact.fromCreationRequest(user, friend);
+    User user = userService.getByUsername(username);
+    User friend = userService.getByUsername(contactUsername);
+    Contact contact = Contact.fromCreationRequest(user, friend);
     return dao.save(contact);
   }
 
   @Override
   public void delete(UUID id) {
-    var username = secUtil.getUsername();
-    var contact = dao.getById(id);
+    String username = secUtil.getUsername();
+    Contact contact = dao.getById(id);
     boolean contactBelongsToAnotherUser = !contact.getUser().getUsername().equals(username);
     if (contactBelongsToAnotherUser) {
-      var message = String.format("Contact %s does not belong to user %s", id, username);
+      String message = String.format("Contact %s does not belong to user %s", id, username);
       throw new ForbiddenException(message);
     
     } else {
