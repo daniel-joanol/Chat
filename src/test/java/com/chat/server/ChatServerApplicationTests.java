cbeb3c195aa9d93f020f9cd44114bfe1eeb79907
Daniel Joanol <joanoldaniel@gmail.com>
2026-08-10 11:25:43 +0200

    Fix tests

--- src/test/java/com/chat/server/ChatServerApplicationTests.java
+++ src/test/java/com/chat/server/ChatServerApplicationTests.java
@@ -2,12 +2,14 @@ package com.chat.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ChatServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
